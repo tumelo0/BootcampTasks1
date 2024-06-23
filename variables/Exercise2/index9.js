48391a4627808f33d52daff73e14217bd7650bd9
Tumelo <tumelo.madume@gmail.com>
2024-06-24 01:04:41 +0200

    Add files via upload
    fifth commit

---
+++ variables/Exercise2/index9.js
@@ -0,0 +1,10 @@
+// Exercise 2: Variable Reassignment
+let favoriteColour = "blue"; // Assign your favorite color to the 'favoriteColour' variable
+console.log("Favorite Color:", favoriteColour);
+
+// Reassign 'favoriteColour' to a different color
+favoriteColour = "green";
+console.log("New Favorite Color:", favoriteColour);
+
+
+
---
+++ variables/Exercise3/index10.js
@@ -0,0 +1,17 @@
+// Exercise 3: Arithmetic Operations
+const num1 = 20; 
+const num2 = 10; 
+
+// Calculate the sum, difference, product, and quotient
+const sum = num1 + num2;
+const difference = num1 - num2;
+const product = num1 * num2;
+const quotient = num1 / num2;
+
+// Print the results
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+
